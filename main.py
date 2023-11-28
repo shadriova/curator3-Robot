27656d5443ad3a490e8a77adc5f55c00d33be9bf
Diana <150387973+shadriova@users.noreply.github.com>
2023-11-28 22:58:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import telebot
-import random
-
-bot = telebot.TeleBot('6906533346:AAFlQ2Dv-Q0ojkwA4uTfQiFSaUZRwYwAUzw')
-
+bot = telebot.TeleBot('6811968600:AAGMfqWutHwsrlgR1CdtK-fClrf12IbgTT8')
+ 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message('Нихао, аригато', parse_mode='Markdown')
-
-@bot.message_handler(commands=['число'])
+    bot.send_message(message.chat.id, '*добро пожаловать в бот!*', parse_mode='Markdown')
+    
+@bot.message_handler(commands=['greetings'])
 def main(message):
-    bot.send_message(digit=random.randint(1,10))
-
-@bot.message_handler(commands=['дрель'])
+    bot.send_message(message.chat.id, 'Приветствую тебя в моём первом боте!', parse_mode='Markdown')
+    
+    
+@bot.message_handler(commands=['binary code'])
 def main(message):
-    bot.send_message('*ВжжжжжВЖжжжж*')
-
-bot.infinyty_pooling()
+    bot.send_message(message.chat.id, 'двоичный код-данные в виде 1 и 0', parse_mode='Markdown')
+    
+      
+@bot.message_handler(commands=['measurement of information '])
+def main(message):
+    bot.send_message(message.chat.id, 'измерение информации-бит\nбайт\nКилобайт\nМегабайт\nГигобайт\nТерабайт', parse_mode='Markdown')
+    
+    
+@bot.message_handler(commands=['Informatics '])
+def main(message):
+    bot.send_message(message.chat.id, 'информатика-это наука,изучающая автоматическую работу с информацией', parse_mode='Markdown') 
+    
+bot.infinity_polling()
