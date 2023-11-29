b3694a9c9f5c470804b3e48fb80147f77fe69418
Diana <150387973+shadriova@users.noreply.github.com>
2023-11-29 14:37:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import telebot
-bot = telebot.TeleBot('6636808037:AAFmPOlYpuTUdXhvLl2xPkjXF2WzeYtDbrU')
+
+bot = telebot.TeleBot('6483708701:AAFJoAVOXeGnc-uoJqoY2p9ps3DOX9Mboxo')
+
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, '*ПРИВЕТ, БОЛЕЛЬЩИК*', parse_mode='Markdown')
+    bot.send_message(message.chat.id, '*Погнали!)*', parse_mode='Markdown')
 
-@bot.message_handler(commands=['goool'])
-def main(message):
-    bot.send_message(message.chat.id, '*ОЛЕ-ОЛЕ-ОЛЕ*', parse_mode='Markdown')
 
-@bot.message_handler(commands=['link'])
+@bot.message_handler(commands=['happy'])
 def main(message):
-    bot.send_message(message.chat.id, '[ссылка](https://rfs.ru/)', parse_mode='Markdown')
+    bot.send_message(message.chat.id, '_Ура!_', parse_mode='Markdown')
+
+
+@bot.message_handler(commands=['sad'])
+def main(message):
+    bot.send_message(message.chat.id, '=(', parse_mode='Markdown')
+
+
+@bot.message_handler(commands=['buy'])
+def main(message):
+    bot.send_message(message.chat.id, 'Пока!', parse_mode='Markdown')
+
 
 bot.infinity_polling()
