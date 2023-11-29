59c6532921498cae4ef6ab94dac0cdc0946b744f
Diana <150387973+shadriova@users.noreply.github.com>
2023-11-29 14:08:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import telebot
-import random
 
-bot = telebot.TeleBot('6906533346:AAFlQ2Dv-Q0ojkwA4uTfQiFSaUZRwYwAUzw')
+bot = telebot.TeleBot('6187203002:AAFqQ9JCdI3WCG0I9PuoOIhgd2P8ZkyvZJY')
+
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message('Нихао, аригато', parse_mode='Markdown')
+    bot.send_message(message.chat.id, 'Привет /n наконец-то ты нашёл меня')
 
-@bot.message_handler(commands=['число'])
+
+@bot.message_handler(commands=['love'])
 def main(message):
-    bot.send_message(digit=random.randint(1,10))
+    bot.send_message(message.chat.id, 'Один честны разговор - может решить много проблем')
 
-@bot.message_handler(commands=['дрель'])
+
+@bot.message_handler(commands=['friend'])
 def main(message):
-    bot.send_message('*ВжжжжжВЖжжжж*')
+    bot.send_message(message.chat.id, 'Любовь может обойтись без взаимности, но дружба — никогда')
 
-bot.infinyty_pooling()
+
+@bot.message_handler(commands=['family'])
+def main(message):
+    bot.send_message(message.chat.id, 'Семья – это место силы')
+
+
+@bot.message_handler(commands=['career'])
+def main(message):
+    bot.send_message(message.chat.id, 'Страх высоты многим мешает делать карьеру… ')
+
+
+bot.infinity_polling()
